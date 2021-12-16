c8eb4586b0ae63f785d9c65fab2742026b9bc970
ZackeryT <90658833+ZackeryT@users.noreply.github.com>
2021-12-16 12:52:21 -0500

    gfh

--- api/js/main.js
+++ api/js/main.js
@@ -1,8 +1,9 @@
-import "../styles/style.css";
+import("../styles/style.css");
+
 async function getData() {
   try {
     const result = await fetch(
-      `https://na1.api.riotgames.com/lol/platform/v3/champion-rotations?RGAPI-cca58d06-1eea-4731-9f87-3f7d66a5e17c`
+      `https://na1.api.riotgames.com/lol/platform/v3/champion-rotations?api_key=RGAPI-cca58d06-1eea-4731-9f87-3f7d66a5e17c`
     );
     const data = await result.json();
     console.log(data);
