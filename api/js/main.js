a204c971e5f344e1c40f1b55f97354f5199f72ac
ZackeryT <90658833+ZackeryT@users.noreply.github.com>
2021-12-16 12:31:02 -0500

    asd

--- api/js/main.js
+++ api/js/main.js
@@ -1,6 +1,13 @@
-import './style.css'
-
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+import "./style.css";
+async function getData() {
+  try {
+    const result = await fetch(
+      `https://na1.api.riotgames.com/lol/platform/v3/champion-rotations?RGAPI-cca58d06-1eea-4731-9f87-3f7d66a5e17c`
+    );
+    const data = await result.json();
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+}
+getData();
