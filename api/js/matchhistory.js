ac376be9c2ec91f4c90ad11a51a1eb467ed2e275
ZackeryT <90658833+ZackeryT@users.noreply.github.com>
2021-12-20 13:58:25 -0500

    a

--- api/js/matchhistory.js
+++ api/js/matchhistory.js
@@ -1,10 +1,11 @@
 import { DOMSelectors } from "./dom";
 
 const matchHistory = JSON.parse(window.localStorage.getItem("matches"));
+
 console.log(matchHistory);
 matchHistory.forEach((item) =>
-	DOMSelectors.matchHistory.insertAdjacentHTML(
-		"beforeend",
-		`<p>${item.info.gameDuration}</p>`
-	)
+  DOMSelectors.matchHistory.insertAdjacentHTML(
+    "beforeend",
+    `<p>${item.info.gameDuration} seconds</p>`
+  )
 );
