02477021ce8f2ea43239412582ab6f53d5bf86f6
KingJelloX <alfredhuang101@icloud.com>
2021-12-19 22:12:05 -0500

    Squashed commit of the following:
    commit f8b5342b68d567e3ffd5d7609116d3ec5568a4da
    Author: KingJelloX <alfredhuang101@icloud.com>
    Date:   Sun Dec 19 22:11:50 2021 -0500
    
        IT WORKSS THE STUPID FILES WORK
    
    commit d8dd135fe6b49ee0bf778345cff011ecceec736c
    Author: KingJelloX <alfredhuang101@icloud.com>
    Date:   Sun Dec 19 21:17:09 2021 -0500
    
        local storage changing html

---
+++ api/js/matchhistory.js
@@ -0,0 +1,10 @@
+import { DOMSelectors } from "./dom";
+
+const matchHistory = JSON.parse(window.localStorage.getItem("matches"));
+console.log(matchHistory);
+matchHistory.forEach((item) =>
+	DOMSelectors.matchHistory.insertAdjacentHTML(
+		"beforeend",
+		`<p>${item.info.gameDuration}</p>`
+	)
+);
