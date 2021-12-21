1a19972fd88b328a67b6f2d611f29a2a6afead52
KingJelloX <alfredhuang101@icloud.com>
2021-12-21 13:58:09 -0500

    check api

--- api/js/loadingScreen.js
+++ api/js/loadingScreen.js
@@ -4,28 +4,39 @@ const summoner = window.localStorage.getItem("searchData");
 
 async function getPuuid() {
 	try {
-		const apiPuuid = await fetch(
-			`https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${summoner}?api_key=RGAPI-6468f5ee-26e6-40c1-b48a-9a75377566bd`
-		).then((api) => api.json());
+		const checkPromise = new Promise(function (resolve, reject) {
+			const apiPuuid = await fetch(
+				`https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${summoner}?api_key=RGAPI-0c154a56-dc27-47c8-a57c-776915c7efae`
+			).then((api) => api.json());
+
+			resolve();
+			reject();
+		});
+
+		checkPromise.then(
+			function (value) {},
+			function (error) {}
+		);
+
 		const puuid = Object.values(apiPuuid)[2];
 		const icon = Object.values(apiPuuid)[4];
 
 		async function grabIcon() {
 			const icons = await fetch(
 				`http://ddragon.leagueoflegends.com/cdn/11.24.1/img/profileicon/${icon}.png`
-			).then((icon = icon.json));
+			).then((icon) => icon.json());
 			window.localStorage.setItem("icons", JSON.stringify(icons, null, 4));
 		}
 		async function getAccount() {
 			try {
 				const apiAccount = await fetch(
-					`https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/${puuid}/ids?start=0&count=20&api_key=RGAPI-6468f5ee-26e6-40c1-b48a-9a75377566bd`
+					`https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/${puuid}/ids?start=0&count=20&api_key=RGAPI-0c154a56-dc27-47c8-a57c-776915c7efae`
 				).then((api) => api.json());
 
 				let apiMatches = [];
 				for (const matchID of apiAccount) {
 					const apiMatch = await fetch(
-						`https://americas.api.riotgames.com/lol/match/v5/matches/${matchID}?api_key=RGAPI-6468f5ee-26e6-40c1-b48a-9a75377566bd`
+						`https://americas.api.riotgames.com/lol/match/v5/matches/${matchID}?api_key=RGAPI-0c154a56-dc27-47c8-a57c-776915c7efae`
 					).then((api) => api.json());
 					console.log(apiMatch);
 					apiMatches.push(apiMatch);
