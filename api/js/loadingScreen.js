4027792d5f3c6d01fbf0f269617e5ed7b4b71833
ZackeryT <90658833+ZackeryT@users.noreply.github.com>
2021-12-23 13:30:03 -0500

    asd

--- api/js/loadingScreen.js
+++ api/js/loadingScreen.js
@@ -4,25 +4,24 @@ const summoner = window.localStorage.getItem("searchData");
 async function getPuuid() {
   try {
     const apiPuuid = await fetch(
-      `https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${summoner}?api_key=RGAPI-70c31577-3dbc-41dd-aab5-ea81e60e6ae0`
+      `https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${summoner}?api_key=RGAPI-c64a2867-0270-43a8-a835-67e05261bc8f`
     ).then((api) => api.json());
     const puuid = Object.values(apiPuuid)[2];
     const icon = Object.values(apiPuuid)[4];
 
-    export const grabIcon = {
-      icon: `http://ddragon.leagueoflegends.com/cdn/11.24.1/img/profileicon/${icon}.png`,
-    };
-
+    const grabIcon = 
+    `http://ddragon.leagueoflegends.com/cdn/11.24.1/img/profileicon/${icon}.png`;
+    window.localStorage.setItem("icon", grabIcon);
     async function getAccount() {
       try {
         const apiAccount = await fetch(
-          `https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/${puuid}/ids?start=0&count=20&api_key=RGAPI-70c31577-3dbc-41dd-aab5-ea81e60e6ae0`
+          `https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/${puuid}/ids?start=0&count=20&api_key=RGAPI-c64a2867-0270-43a8-a835-67e05261bc8f`
         ).then((api) => api.json());
 
         let apiMatches = [];
         for (const matchID of apiAccount) {
           const apiMatch = await fetch(
-            `https://americas.api.riotgames.com/lol/match/v5/matches/${matchID}?api_key=RGAPI-70c31577-3dbc-41dd-aab5-ea81e60e6ae0`
+            `https://americas.api.riotgames.com/lol/match/v5/matches/${matchID}?api_key=RGAPI-c64a2867-0270-43a8-a835-67e05261bc8f`
           ).then((api) => api.json());
           console.log(apiMatch);
           apiMatches.push(apiMatch);
--- api/js/matchhistory.js
+++ api/js/matchhistory.js
@@ -1,12 +1,17 @@
 import { DOMSelectors } from "./dom";
-import { grabIcon } from "./loadingScreen";
 
 const matchHistory = JSON.parse(window.localStorage.getItem("matches"));
+const picture = window.localStorage.getItem("icon");
+
 console.log(matchHistory);
+
 matchHistory.forEach((item) =>
   DOMSelectors.matchHistory.insertAdjacentHTML(
     "beforeend",
     `<p>${item.info.gameDuration} seconds</p>`
   )
 );
-grabIcon.insertAdjacentHTML("beforeend", `<image>${grabIcon}</image>`);
+DOMSelectors.matchHistory.insertAdjacentHTML(
+  "beforeend",
+  `<image src="${picture}"></image>`
+);
