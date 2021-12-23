616bfddd5394c628da68afd986695ed9a1ded4bc
KingJelloX <alfredhuang101@icloud.com>
2021-12-23 02:19:30 -0500

    made icon work

--- api/js/loadingScreen.js
+++ api/js/loadingScreen.js
@@ -4,39 +4,26 @@ const summoner = window.localStorage.getItem("searchData");
 
 async function getPuuid() {
 	try {
-		const checkPromise = new Promise(function (resolve, reject) {
-			const apiPuuid = await fetch(
-				`https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${summoner}?api_key=RGAPI-0c154a56-dc27-47c8-a57c-776915c7efae`
-			).then((api) => api.json());
-
-			resolve();
-			reject();
-		});
-
-		checkPromise.then(
-			function (value) {},
-			function (error) {}
+		const getIgn = await fetch(
+			`https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${summoner}?api_key=RGAPI-0d966c91-f3e4-4aa6-bd27-00b3d8c2861b`
 		);
+		const apiPuuid = await getIgn.json();
 
 		const puuid = Object.values(apiPuuid)[2];
 		const icon = Object.values(apiPuuid)[4];
 
-		async function grabIcon() {
-			const icons = await fetch(
-				`http://ddragon.leagueoflegends.com/cdn/11.24.1/img/profileicon/${icon}.png`
-			).then((icon) => icon.json());
-			window.localStorage.setItem("icons", JSON.stringify(icons, null, 4));
-		}
+		window.localStorage.setItem("icon", icon);
+
 		async function getAccount() {
 			try {
 				const apiAccount = await fetch(
-					`https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/${puuid}/ids?start=0&count=20&api_key=RGAPI-0c154a56-dc27-47c8-a57c-776915c7efae`
+					`https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/${puuid}/ids?start=0&count=20&api_key=RGAPI-0d966c91-f3e4-4aa6-bd27-00b3d8c2861b`
 				).then((api) => api.json());
 
 				let apiMatches = [];
 				for (const matchID of apiAccount) {
 					const apiMatch = await fetch(
-						`https://americas.api.riotgames.com/lol/match/v5/matches/${matchID}?api_key=RGAPI-0c154a56-dc27-47c8-a57c-776915c7efae`
+						`https://americas.api.riotgames.com/lol/match/v5/matches/${matchID}?api_key=RGAPI-0d966c91-f3e4-4aa6-bd27-00b3d8c2861b`
 					).then((api) => api.json());
 					console.log(apiMatch);
 					apiMatches.push(apiMatch);
@@ -52,8 +39,9 @@ async function getPuuid() {
 			}
 		}
 		getAccount();
-		grabIcon();
 	} catch (error) {
+		//window.location.href = "./error.html";
+		console.log("Not valid summonor name");
 		console.log(error);
 	}
 }
--- api/js/matchhistory.js
+++ api/js/matchhistory.js
@@ -1,8 +1,18 @@
 import { DOMSelectors } from "./dom";
 
 const matchHistory = JSON.parse(window.localStorage.getItem("matches"));
+const summonerIcon = window.localStorage.getItem("icon");
 
 console.log(matchHistory);
+console.log(summonerIcon);
+
+DOMSelectors.matchHistory.insertAdjacentHTML(
+	"afterbegin",
+	`<div>
+			<img src="http://ddragon.leagueoflegends.com/cdn/11.24.1/img/profileicon/${summonerIcon}.png" alt="Summonor icon">
+		</div>`
+);
+
 matchHistory.forEach((item) =>
 	DOMSelectors.matchHistory.insertAdjacentHTML(
 		"beforeend",
