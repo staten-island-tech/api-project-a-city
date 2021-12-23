3743d53d100fde3ed73e4bb50baaa82820a24119
KingJelloX <alfredhuang101@icloud.com>
2021-12-23 14:06:28 -0500

    styling rn

--- api/js/main.js
+++ api/js/main.js
@@ -3,7 +3,10 @@ import { DOMSelectors } from "./dom";
 
 DOMSelectors.form.addEventListener("submit", function (e) {
 	e.preventDefault();
-	const summoner = DOMSelectors.summonerName.value;
-	localStorage.setItem("searchData", summoner);
-	window.location.href = "./html/loadingScreen.html";
+	const summoner = DOMSelectors.summonerName.value.replace(/\s/g, "");
+	if (summoner === "") {
+	} else {
+		localStorage.setItem("searchData", summoner);
+		window.location.href = "./html/loadingScreen.html";
+	}
 });
