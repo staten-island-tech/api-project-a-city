263c19c6696e80577200ac1015f0c6f07bcbc8dd
KingJelloX <alfredhuang101@icloud.com>
2021-12-23 17:34:44 -0500

    Squashed commit of the following:
    commit 7090151485153d503b210647a30013a3a66637af
    Author: KingJelloX <alfredhuang101@icloud.com>
    Date:   Thu Dec 23 17:34:30 2021 -0500
    
        CLEAN UI
    
    commit 8212e99df1414c9e7604e532f2beadc9e008632a
    Author: KingJelloX <alfredhuang101@icloud.com>
    Date:   Thu Dec 23 16:35:18 2021 -0500
    
        main page done
    
    commit e9ca1f12eb99f174b4cf61f054622e3dc1bd0952
    Merge: 3743d53 28cce51
    Author: KingJelloX <alfredhuang101@icloud.com>
    Date:   Thu Dec 23 14:14:16 2021 -0500
    
        Merge branch 'Development' into Alfred
    
    commit 3743d53d100fde3ed73e4bb50baaa82820a24119
    Author: KingJelloX <alfredhuang101@icloud.com>
    Date:   Thu Dec 23 14:06:28 2021 -0500
    
        styling rn
    
    commit 616bfddd5394c628da68afd986695ed9a1ded4bc
    Author: KingJelloX <alfredhuang101@icloud.com>
    Date:   Thu Dec 23 02:19:30 2021 -0500
    
        made icon work
    
    commit 1a19972fd88b328a67b6f2d611f29a2a6afead52
    Author: KingJelloX <alfredhuang101@icloud.com>
    Date:   Tue Dec 21 13:58:09 2021 -0500
    
        check api

--- api/js/matchhistory.js
+++ api/js/matchhistory.js
@@ -2,23 +2,22 @@ import { DOMSelectors } from "./dom";
 import("../styles/matchHistory.css");
 
 const matchHistory = JSON.parse(window.localStorage.getItem("matches"));
+
 const picture = window.localStorage.getItem("icon");
 const level = window.localStorage.getItem("level");
 const name = window.localStorage.getItem("name");
-console.log(name);
 
 DOMSelectors.matchHistory.insertAdjacentHTML(
-  "beforeend",
-  `<image src="${picture}" class="player"></image>
-  <h2 class="player">Summoner Name: ${name}</h2>
-  <h2 class="player">Champion Level: ${level}</h2>
-  <h2>Match Duration (in seconds):</h2>`
+	"beforeend",
+	`<image src="${picture}" class="player"></image>
+	<h2 class="player">Summoner Name: ${name}</h2>
+	<h2 class="player">Champion Level: ${level}</h2>
+	<h2>Match Duration (in seconds):</h2>`
 );
 
 matchHistory.forEach((item) =>
-  DOMSelectors.matchHistory.insertAdjacentHTML(
-    "beforeend",
-
-    `<p>${item.info.gameDuration} seconds</p>`
-  )
+	DOMSelectors.matchHistory.insertAdjacentHTML(
+		"beforeend",
+		`<p>${item.info.gameDuration} seconds</p>`
+	)
 );
