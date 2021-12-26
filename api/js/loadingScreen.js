84aec5e1ba0e76b1bf80b72cf76d4a612674d4cc
KingJelloX <alfredhuang101@icloud.com>
2021-12-26 17:58:11 -0500

    FINSIHED SUMMONER SPELLS/RUNES

--- api/js/loadingScreen.js
+++ api/js/loadingScreen.js
@@ -1,6 +1,7 @@
 import("../styles/load.css");
 
 const summoner = window.localStorage.getItem("searchData");
+
 async function getPuuid() {
 	try {
 		const apiPuuid = await fetch(
@@ -14,13 +15,13 @@ async function getPuuid() {
 		window.localStorage.setItem("level", level);
 		window.localStorage.setItem("name", name);
 
-		const grabIcon = `http://ddragon.leagueoflegends.com/cdn/11.24.1/img/profileicon/${icon}.png`;
-		window.localStorage.setItem("icon", grabIcon);
+		const summonerIcon = `http://ddragon.leagueoflegends.com/cdn/11.24.1/img/profileicon/${icon}.png`;
+		window.localStorage.setItem("icon", summonerIcon);
 
 		async function getAccount() {
 			try {
 				const apiAccount = await fetch(
-					`https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/${puuid}/ids?start=0&count=5&api_key=RGAPI-ea07fd1f-ddfc-4538-a971-1df4a679d74b`
+					`https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/${puuid}/ids?start=0&count=25&api_key=RGAPI-ea07fd1f-ddfc-4538-a971-1df4a679d74b`
 				).then((api) => api.json());
 
 				let apiMatches = [];
@@ -31,46 +32,8 @@ async function getPuuid() {
 					).then((api) => api.json());
 					apiMatches.push(apiMatch);
 				}
-
-				const match = JSON.parse(JSON.stringify(apiMatches, null, 4));
-
-				match.forEach(function (item) {
-					const info = item.info;
-					const metaData = info.participants.filter((summoner) => summoner.summonerName === name)[0];
-
-					async function getSpell() {
-						const spellJson = await fetch("http://ddragon.leagueoflegends.com/cdn/11.24.1/data/en_US/summoner.json")
-							.then((response) => response.json())
-							.then((spell) => Object.values(spell.data));
-
-						const findspell1 = spellJson.filter((spell) => spell.key == metaData.summoner1Id);
-						const spell1 = findspell1[0].id;
-						console.log(spell1);
-
-						const findspell2 = spellJson.filter((spell) => spell.key == metaData.summoner2Id);
-						const spell2 = findspell2[0].id;
-						console.log(spell2);
-					}
-					getSpell();
-
-					async function getRune() {
-						const runeJson = await fetch("http://ddragon.leagueoflegends.com/cdn/11.15.1/data/en_US/runesReforged.json").then((response) => response.json());
-
-						const rune1Type = runeJson.filter((rune) => rune.id == metaData.perks.styles[0].style)[0];
-						const filterRune1 = rune1Type.slots[0].runes;
-
-						const rune2Type = runeJson.filter((rune) => rune.id == metaData.perks.styles[1].style)[0];
-						const filterRune2 = rune2Type.icon;
-						console.log(filterRune2);
-
-						const getRune1 = filterRune1.filter((rune) => rune.id == metaData.perks.styles[0].selections[0].perk)[0].icon;
-						console.log(getRune1);
-					}
-					getRune();
-				});
-
-				console.log("Page LOADED");
 				window.localStorage.setItem("matches", JSON.stringify(apiMatches, null, 4));
+
 				window.location.href = "./matchHistory.html";
 			} catch (error) {
 				console.log(error);
--- api/js/matchhistory.js
+++ api/js/matchhistory.js
@@ -64,103 +64,108 @@ function insert_combos(combos) {
 	return inner;
 }
 
-async function getRune() {
-	const runeJson = await fetch("http://ddragon.leagueoflegends.com/cdn/11.15.1/data/en_US/runesReforged.json").then((response) => response.json());
-
-	const rune1Type = runeJson.filter((rune) => rune.id == metaData.perks.styles[0].style)[0];
-	const filterRune1 = rune1Type.slots[0].runes;
-
-	const rune2Type = runeJson.filter((rune) => rune.id == metaData.perks.styles[1].style)[0];
-	const filterRune2 = rune2Type.icon;
-
-	const getRune1 = filterRune1.filter((rune) => rune.id == metaData.perks.styles[0].selections[0].perk)[0].icon;
-	console.log(getRune1);
-	console.log(filterRune2);
-}
-
 match.forEach(function (item) {
 	const info = item.info;
 	const metaData = info.participants.filter((summoner) => summoner.summonerName === name)[0];
-
 	const items = [metaData.item0, metaData.item1, metaData.item2, metaData.item3, metaData.item4, metaData.item5];
 	const combos = [metaData.doubleKills, metaData.tripleKills, metaData.quadraKills, metaData.pentaKills];
 
 	async function getSpell1() {
-		const spellJson = await fetch("http://ddragon.leagueoflegends.com/cdn/11.24.1/data/en_US/summoner.json")
+		const spell1 = await fetch("http://ddragon.leagueoflegends.com/cdn/11.24.1/data/en_US/summoner.json")
 			.then((response) => response.json())
-			.then((spell) => Object.values(spell.data));
+			.then((spell) => Object.values(spell.data))
+			.then((spell) => spell.filter((spell) => spell.key == metaData.summoner1Id))
+			.then((spell) => spell[0].id);
 
-		const findspell1 = spellJson.filter((spell) => spell.key == metaData.summoner1Id);
-		const spell1 = findspell1[0].id;
-		return spell1;
+		async function getSpell2() {
+			const spell2 = await fetch("http://ddragon.leagueoflegends.com/cdn/11.24.1/data/en_US/summoner.json")
+				.then((response) => response.json())
+				.then((spell) => Object.values(spell.data))
+				.then((spell) => spell.filter((spell) => spell.key == metaData.summoner2Id))
+				.then((spell) => spell[0].id);
+
+			return spell2;
+		}
+
+		async function getRune1() {
+			const rune1 = await fetch("http://ddragon.leagueoflegends.com/cdn/11.15.1/data/en_US/runesReforged.json")
+				.then((response) => response.json())
+				.then((rune) => rune.filter((rune) => rune.id == metaData.perks.styles[0].style)[0])
+				.then((rune) => rune.slots[0].runes)
+				.then((rune) => rune.filter((rune) => rune.id == metaData.perks.styles[0].selections[0].perk)[0].icon);
+
+			return rune1;
+		}
+
+		async function getRune2() {
+			const rune2 = await fetch("http://ddragon.leagueoflegends.com/cdn/11.15.1/data/en_US/runesReforged.json")
+				.then((response) => response.json())
+				.then((rune) => rune.filter((rune) => rune.id == metaData.perks.styles[1].style)[0])
+				.then((rune) => rune.icon);
+
+			return rune2;
+		}
+
+		getSpell2().then(function (spell2) {
+			DOMSelectors.matchHistory.insertAdjacentHTML(
+				"beforeend",
+				`<div class="matchHistory-data ${metaData.win}">
+					<div class="matchHistoryDataSetTop">
+						<p>${info.gameMode}</p>
+						<p>${fancyTimeFormat(info.gameDuration)}</p>
+					</div>
+					<div class="matchHistoryDataSetMid">
+						<div class="matchHistoryDataSetMidChamp">
+							<div class="matchHistoryDataSetMidChampImg">
+								<img class="champIcon" src="https://ddragon.leagueoflegends.com/cdn/11.24.1/img/champion/${metaData.championName}.png" />
+								<div class="matchHistoryDataSetMidChampImgSpells">
+										<img class="spellsImg" src="http://ddragon.leagueoflegends.com/cdn/11.24.1/img/spell/${spell1}.png" />
+										<img class="spellsImg" src="http://ddragon.leagueoflegends.com/cdn/11.24.1/img/spell/${spell2}.png" />
+								</div>
+									<div id=${info.gameId} class="matchHistoryDataSetMidChampImgRunes">
+								</div>
+							</div>
+							<p>Level ${metaData.champLevel}</p>
+						</div>
+						<div class="matchHistoryDataSetMidStats">
+							<div>
+								<p>${metaData.kills}/${metaData.deaths}/${metaData.assists}</p>
+							</div>
+							<div>
+								<div class="matchHistoryDataSetMidStatsCs">
+									<p>${metaData.totalMinionsKilled} cs</p>
+									<p>${(metaData.totalMinionsKilled / (info.gameDuration / 60)).toFixed(1)} cs/min</p>
+								</div>
+							</div>
+						</div>
+						<div class="matchHistoryDataSetMidInv">
+							<div class="matchHistoryDataSetMidItems">
+								${insert_img(items)}
+							</div>
+							<div>
+								<img class="items" src="http://ddragon.leagueoflegends.com/cdn/11.24.1/img/item/${metaData.item6}.png">
+							</div>
+						</div>
+					</div>
+					<div>
+						<div class="matchHistoryDataSetBotCombos">
+							<div class="matchHistoryDataSetBotCombosKills">${insert_combos(combos)}</div>
+								<div class="timeSpentDead">
+								<p>Time Dead: ${fancyTimeFormat(metaData.totalTimeSpentDead)} <p>
+							</div>
+						</div>
+					</div>
+				</div>`
+			);
+
+			let runes = document.getElementById(info.gameId);
+			getRune1().then(function (rune1) {
+				runes.insertAdjacentHTML("beforeend", `<img class="runesImg" src="https://ddragon.canisback.com/img/${rune1}" />`);
+			});
+			getRune2().then(function (rune2) {
+				runes.insertAdjacentHTML("beforeend", `<img class="runesImg" src="https://ddragon.canisback.com/img/${rune2}" />`);
+			});
+		});
 	}
 	getSpell1();
-
-	async function getSpell2() {
-		const spellJson = await fetch("http://ddragon.leagueoflegends.com/cdn/11.24.1/data/en_US/summoner.json")
-			.then((response) => response.json())
-			.then((spell) => Object.values(spell.data));
-
-		const findspell2 = spellJson.filter((spell) => spell.key == metaData.summoner2Id);
-		const spell2 = findspell2[0].id;
-		return spell2;
-	}
-	getSpell2();
-
-	DOMSelectors.matchHistory.insertAdjacentHTML(
-		"beforeend",
-		`<div class="matchHistory-data ${metaData.win}">
-    <div class="matchHistoryDataSetTop">
-        <p>${info.gameMode}</p>
-        <p>${fancyTimeFormat(info.gameDuration)}</p>
-    </div>
-    <div class="matchHistoryDataSetMid">
-        <div class="matchHistoryDataSetMidChamp">
-            <div class="matchHistoryDataSetMidChampImg">
-				<img class="champIcon" src="https://ddragon.leagueoflegends.com/cdn/11.24.1/img/champion/${metaData.championName}.png" />
-				<div class="matchHistoryDataSetMidChampImgSpells">
-					<img class="spellsImg" src="http://ddragon.leagueoflegends.com/cdn/11.24.1/img/spell/SummonerBoost.png" />
-					<img class="spellsImg" src="http://ddragon.leagueoflegends.com/cdn/11.24.1/img/spell/SummonerBoost.png" />
-				</div>
-				<div class="matchHistoryDataSetMidChampImgRunes">
-					<img class="runesImg" src="http://ddragon.leagueoflegends.com/cdn/11.24.1/img/spell/SummonerBoost.png" />
-					<img class="runesImg" src="http://ddragon.leagueoflegends.com/cdn/11.24.1/img/spell/SummonerBoost.png" />
-				</div>
-			<div>
-		</div>
-	</div>
-
-            <p>Level ${metaData.champLevel}</p>
-        </div>
-        <div class="matchHistoryDataSetMidStats">
-            <div>
-                <p>${metaData.kills}/${metaData.deaths}/${metaData.assists}</p>
-            </div>
-            <div>
-                <div class="matchHistoryDataSetMidStatsCs">
-                    <p>${metaData.totalMinionsKilled} cs</p>
-                    <p>${(metaData.totalMinionsKilled / (info.gameDuration / 60)).toFixed(1)} cs/min</p>
-                </div>
-            </div>
-        </div>
-        <div class="matchHistoryDataSetMidInv">
-            <div class="matchHistoryDataSetMidItems">
-                ${insert_img(items)}
-            </div>
-			<div>
-				<img class="items" src="http://ddragon.leagueoflegends.com/cdn/11.24.1/img/item/${metaData.item6}.png">
-			</div>
-        </div>
-    </div>
-    <div>
-        <div class="matchHistoryDataSetBotCombos">
-            <div class="matchHistoryDataSetBotCombosKills">${insert_combos(combos)}</div>
-			<div class="timeSpentDead">
-            	<p>Time Dead: ${fancyTimeFormat(metaData.totalTimeSpentDead)} <p>
-        </div>
-        </div>
-        
-    </div>
-</div>`
-	);
 });
