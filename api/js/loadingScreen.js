80d4723b614c92477873e8e978fd33f1cc420560
KingJelloX <alfredhuang101@icloud.com>
2022-01-03 13:57:35 -0500

    update keys

--- api/js/loadingScreen.js
+++ api/js/loadingScreen.js
@@ -5,7 +5,7 @@ const summoner = window.localStorage.getItem("searchData");
 async function getPuuid() {
 	try {
 		const apiPuuid = await fetch(
-			`https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${summoner}?api_key=RGAPI-e83de3af-039e-4dec-ab50-0f66b7a61038`
+			`https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${summoner}?api_key=RGAPI-e8c736fb-024d-41c0-ab2e-1174795d3cf8`
 		).then(api => api.json());
 		const puuid = Object.values(apiPuuid)[2];
 		const icon = Object.values(apiPuuid)[4];
@@ -21,14 +21,14 @@ async function getPuuid() {
 		async function getAccount() {
 			try {
 				const apiAccount = await fetch(
-					`https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/${puuid}/ids?start=0&count=25&api_key=RGAPI-e83de3af-039e-4dec-ab50-0f66b7a61038`
+					`https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/${puuid}/ids?start=0&count=25&api_key=RGAPI-e8c736fb-024d-41c0-ab2e-1174795d3cf8`
 				).then(api => api.json());
 
 				let apiMatches = [];
 
 				for (const matchID of apiAccount) {
 					const apiMatch = await fetch(
-						`https://americas.api.riotgames.com/lol/match/v5/matches/${matchID}?api_key=RGAPI-e83de3af-039e-4dec-ab50-0f66b7a61038`
+						`https://americas.api.riotgames.com/lol/match/v5/matches/${matchID}?api_key=RGAPI-e8c736fb-024d-41c0-ab2e-1174795d3cf8`
 					).then(api => api.json());
 					apiMatches.push(apiMatch);
 				}
